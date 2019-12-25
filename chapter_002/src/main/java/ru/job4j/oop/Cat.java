b2102ce271f93d535280067ce5996d17cb67b08d
alexey-belov1 <veshousis1@gmail.com>
2019-12-25 23:24:34 +0500

    Часть 002. ООП. 1.5. Поля объекта

---
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println("There are " + this.name + "'s food. " +  this.food);
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("gav");
+        gav.eat("kotleta");
+        gav.show();
+    }
+}
