fa72778fca10a35d5b5dd36afd36749fb747ebdd
alexey-belov1 <veshousis1@gmail.com>
2019-12-28 21:47:52 +0500

    Часть 002. ООП. 5. Аннотация @Override

--- chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,6 +1,7 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         return String.format("{\n name: %s \n body: %s \n}", name, body);
     }
