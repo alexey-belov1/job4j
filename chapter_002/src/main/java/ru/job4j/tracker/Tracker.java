1b0bc7caa5f7e921e3725674a3af622725c29ce3
alexey-belov1 <veshousis1@gmail.com>
2019-12-29 01:56:31 +0500

    Часть 002. ООП. 5. Реализовать класс Tracker

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,89 @@
 package ru.job4j.tracker;
 
+import java.util.Random;
+import java.util.Arrays;
+
+/**
+ * @version $Id$
+ * @since 0.1
+ */
 public class Tracker {
+    /**
+     * Массив для хранение заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод реализующий добавление заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[this.position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rnd = new Random();
+        return String.valueOf(System.currentTimeMillis() + rnd.nextLong());
+    }
+
+    /**
+     * Метод возвращает список всех заявок
+     * @return Все заявки
+     */
+    public Item[] findAll() {
+        int size = 0;
+        Item[] itemsWithoutNull = new Item[items.length];
+        for (int i = 0; i < position; i++) {
+            if (items[i].getId() != null) {
+                itemsWithoutNull[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemsWithoutNull, size);
+    }
+
+    /**
+     * Метод возвращает все заявки с указанным именем
+     * @param key - Имя в заявке
+     * @return Все заявки с указанным именем
+     */
+    public Item[] findByName(String key) {
+        int size = 0;
+        Item[] itemsIncludeKey = new Item[items.length];
+        for (int i = 0; i < position; i++) {
+            if (items[i].getName().equals(key)) {
+                itemsIncludeKey[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemsIncludeKey, size);
+    }
+
+    /**
+     * Метод возвращает заявку с указанным ключом. Если такой заявки нет, то возвращает null
+     * @param id - уникальный ключ
+     * @return Заявка с указанным ключом
+     */
+    public Item findById(String id) {
+        Item itemWithId = null;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].getId().equals(id)) {
+                itemWithId = items[i];
+                break;
+            }
+        }
+        return itemWithId;
+    }
 }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenFindAllinTracker() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("test1"));
+        tracker.add(new Item("test2"));
+        tracker.add(new Item("test3"));
+        Item[] result = tracker.findAll();
+        assertThat(result.length, is(3));
+    }
+
+    @Test
+    public void whenfindByNameinTracker() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("test1"));
+        tracker.add(new Item("test2"));
+        tracker.add(new Item("test3"));
+        tracker.add(new Item("test2"));
+        tracker.add(new Item("test4"));
+        Item[] result = tracker.findByName("test2");
+        assertThat(result.length, is(2));
+    }
+}
