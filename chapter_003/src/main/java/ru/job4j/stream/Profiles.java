04ab8da290b364d610749fc9489973f30b3bb725
alexey-belov1 <veshousis1@gmail.com>
2020-01-25 21:53:36 +0500

    Часть 004. 3. Уникальность элементов и сортировка (2)

--- chapter_003/src/main/java/ru/job4j/stream/Profiles.java
+++ chapter_003/src/main/java/ru/job4j/stream/Profiles.java
@@ -7,8 +7,8 @@ public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return  profiles.stream()
                     .map(profile -> profile.getAddress())
-                    .distinct()
                     .sorted((Address first, Address second) -> first.getCity().compareTo(second.getCity()))
+                    .distinct()
                     .collect(Collectors.toList());
     }
 }
