0ce0dec402812013c8ca15250b3829b3d3e59d5f
alexey-belov1 <veshousis1@gmail.com>
2020-02-06 14:12:39 +0500

    Jun. Часть 001. 5.3.0 Создать метод delete для односвязного списка

---
+++ chapter_001_jun/src/test/java/ru/job4j/list/SimpleArrayListTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SimpleArrayListTest {
+
+    private SimpleArrayList<Integer> list;
+
+    @Before
+    public void beforeTest() {
+        list = new SimpleArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetOneResultTwo() {
+        assertThat(list.get(0), is(3));
+        assertThat(list.get(1), is(2));
+        assertThat(list.get(2), is(1));
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetSizeResultThree() {
+        assertThat(list.getSize(), is(3));
+    }
+
+    @Test
+    public void whenDeleteTwoElements() {
+        assertThat(list.delete(), is(3));
+        assertThat(list.delete(), is(2));
+        assertThat(list.get(0), is(1));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenTryDeleteEmpty() {
+        assertThat(list.delete(), is(3));
+        assertThat(list.delete(), is(2));
+        assertThat(list.delete(), is(1));
+        list.delete();
+    }
+}
