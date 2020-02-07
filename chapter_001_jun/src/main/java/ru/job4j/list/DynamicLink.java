fef49720de24a7a340c76a2c5632a1419747c1bc
alexey-belov1 <veshousis1@gmail.com>
2020-02-07 17:45:57 +0500

    Jun. Часть 001. 5.3.2. Создать контейнер на базе связанного списка

---
+++ chapter_001_jun/src/main/java/ru/job4j/list/DynamicLink.java
@@ -0,0 +1,69 @@
+package ru.job4j.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DynamicLink<E> implements Iterable<E> {
+
+    private Node<E> first;
+    private int size = 0;
+    private int modCount = 0;
+
+    //Метод добавляет данные в конец списка
+    public void add(E value) {
+        if (size == 0) {
+           this.first = new Node<>(value);
+        } else {
+           getNode(size - 1).next = new Node<>(value);
+        }
+        size++;
+        modCount++;
+    }
+
+    public E get(int index) {
+        return getNode(index).data;
+    }
+
+    private Node<E> getNode(int index) {
+        Node<E> nodeTemp = this.first;
+        for (int i = 0; i < index; i++) {
+            nodeTemp = nodeTemp.next;
+        }
+        return nodeTemp;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int indexIt = 0;
+            private int modCountIt = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return this.indexIt != size;
+            }
+
+            @Override
+            public E next() {
+                if (this.modCountIt != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return get(this.indexIt++);
+            }
+        };
+    }
+
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+    }
+}
---
+++ chapter_001_jun/src/test/java/ru/job4j/list/DynamicLinkTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DynamicLinkTest {
+    private DynamicLink<Integer> link;
+
+    @Before
+    public void beforeTest() {
+        link = new DynamicLink<Integer>();
+        link.add(1);
+        link.add(2);
+        link.add(3);
+    }
+
+    @Test
+    public void whenAddElementsGetElements() {
+        assertThat(link.get(0), is(1));
+        assertThat(link.get(1), is(2));
+        assertThat(link.get(2), is(3));
+    }
+
+    @Test
+    public void whenCheckIterator() {
+        Iterator<Integer> it = link.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenModCountItFromIteratorNotEqualModCount() {
+        Iterator<Integer> it = link.iterator();
+        assertThat(it.next(), is(1));
+        link.add(4);
+        it.next();
+    }
+}
