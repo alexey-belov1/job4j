3344ce77210abc07674065413bc76c65cfaf3a94
alexey-belov1 <veshousis1@gmail.com>
2020-02-10 23:49:20 +0500

    Jun. Часть 001. 2. Не перекрывать equals hashCode

--- chapter_001_jun/src/main/java/ru/job4j/map/User.java
+++ chapter_001_jun/src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     public String name;
@@ -11,5 +11,17 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+    /*В карту добавлены две записи, поскольку их ключи не равны. В данном случае ключами являются объекты, для которых не переопределены методы hashCode и equals.
+     По умолчанию метод hashCode преобразовывает адрес объекта в целочисленное значение, а метод equals сравнивает адреса объектов в памяти.
+     Объекты имеют разные адреса в памяти, и следовательно не равны друг другу.
+     */
+    public static void main(String[] args) {
+        User user1 = new User("Vasya", 2, new GregorianCalendar(1990, Calendar.JANUARY, 1));
+        User user2 = new User("Vasya", 2, new GregorianCalendar(1990, Calendar.JANUARY, 1));
+        Map<User, Object> users = new HashMap<>();
+        users.put(user1, "first");
+        users.put(user2, "second");
+        System.out.println(users);
     }
 }
