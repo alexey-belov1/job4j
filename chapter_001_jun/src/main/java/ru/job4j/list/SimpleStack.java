84ac9b38339e2de1cf7732aa5a271f26d89467e9
alexey-belov1 <veshousis1@gmail.com>
2020-02-10 18:40:59 +0500

    Jun. Часть 001. 5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack (2)

--- chapter_001_jun/src/main/java/ru/job4j/list/SimpleStack.java
+++ chapter_001_jun/src/main/java/ru/job4j/list/SimpleStack.java
@@ -4,20 +4,23 @@ import java.util.NoSuchElementException;
 
 public class SimpleStack<T> {
     private DynamicLink<T> link = new DynamicLink<>();
-    private int size = 0;
 
     public T poll() {
+        int size = 0;
+        for (T cell : link) {
+            size++;
+        }
+
         if (size == 0) {
             throw new NoSuchElementException();
         }
+
         T data = link.get(size - 1);
         link.remove(size - 1);
-        size--;
         return data;
     }
 
     public void push(T value) {
         link.add(value);
-        size++;
     }
 }
