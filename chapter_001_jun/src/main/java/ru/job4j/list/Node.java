36b68d8f46ccb0fcf0e8fd26bedef55d2b34753a
alexey-belov1 <veshousis1@gmail.com>
2020-02-23 18:48:05 +0500

    Jun. Часть 001. 5.3.4. Задан связанный список. Определить цикличность (2)

--- chapter_001_jun/src/main/java/ru/job4j/list/Node.java
+++ chapter_001_jun/src/main/java/ru/job4j/list/Node.java
@@ -1,7 +1,5 @@
 package ru.job4j.list;
 
-import java.util.NoSuchElementException;
-
 public class Node<T> {
 
     T data;
@@ -11,39 +9,21 @@ public class Node<T> {
         this.data = data;
     }
 
-    /**
-     * Метод, который проверяет цикличность в списке.
-     * В методе используется цикл для движения по элементам списка. Количество итераций цикла сохраняется в переменной beginCount.
-     * На каждой итерации:
-     *      - у текущего узла временно удаляется ссылка на следующий и рассчитывается количество узлов от первого до текущего узла (метод count(Node first));
-     *      - проверяется условие beginCount > count(Node first), выполнение которого говорит о том, что в списке имеется цикличность;
-     *      - ссылка на следующий узел обратно вставляется в текущий узел.
-     */
     public static <E> boolean hasCycle(Node<E> first) {
-        if (first == null) {
-            throw new NoSuchElementException();
-        }
-
         boolean result = false;
-        Node<E> initialFirst = first;
-        int beginCount = 0;
-        while (first.next != null) {
-            beginCount++;
-            Node<E> tempDel = first.next;
-            first.next = null;
-            if (beginCount > Node.count(initialFirst)) {
+        Node<E> node1 = first;
+        Node<E> node2 = first;
+
+        while (node2 != null && node2.next != null) {
+            node1 = node1.next;
+            node2 = node2.next.next;
+
+            if (node1 == node2) {
                 result = true;
-                first.next = tempDel;
                 break;
             }
-            first.next = tempDel;
-            first = first.next;
         }
 
         return result;
-    }
-
-    private static <E> int count(Node<E> first) {
-        return (first.next == null) ? 1 : count(first.next) + 1;
     }
 }
