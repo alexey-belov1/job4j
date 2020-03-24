8848d3db7451c12344ded75df00d1ffb3330c90f
alexey-belov1 <veshousis1@gmail.com>
2020-03-24 19:37:51 +0500

    Jun. Часть 002. 1. Бот (2)

--- chapter_002_jun/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002_jun/src/main/java/ru/job4j/io/EchoServer.java
@@ -3,62 +3,28 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class EchoServer {
 
-    private String getMsg(String text) {
-        String result = "";
-        Pattern pattern = Pattern.compile("^\\?msg=(\\S*)");
-        Matcher matcher = pattern.matcher(text);
-
-        if (matcher.find()) {
-            result = matcher.group().split("=")[1];
-        }
-        return result;
-    }
-
-    private String getRequest(String text) {
-        String result = "";
-        Pattern pattern = Pattern.compile("/(\\S*)");
-        Matcher matcher = pattern.matcher(text);
-
-        if (matcher.find()) {
-            result = matcher.group().substring(1);
-        }
-        return result;
-    }
-
-
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            while (true) {
+            while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
 
-                    in.mark(1);
-                    String str = in.readLine();
-                    while (!str.isEmpty()) {
-                        System.out.println(str);
-                        str = in.readLine();
-                    }
-
-                    in.reset();
-                    String request = new EchoServer().getRequest(in.readLine());
-                    String msg = new EchoServer().getMsg(request);
+                    String msg = in.readLine();
+                    int index = msg.indexOf("?msg=");
+                    msg = (index != -1) ? msg.substring(index + 5).split(" ")[0] : "";
 
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     if (msg.equals("Exit")) {
-                        break;
+                        server.close();
                     } else if (msg.equals("Hello")) {
-                        out.write("Hello\n".getBytes());
-                    } else if (!msg.isEmpty()) {
-                        out.write((msg + "\n").getBytes());
+                        out.write("Hello\r\n".getBytes());
                     } else {
-                        out.write((request + "\n").getBytes());
+                        out.write((msg + "\r\n").getBytes());
                     }
                 }
             }
