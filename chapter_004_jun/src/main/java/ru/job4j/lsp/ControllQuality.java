dfebc0758760922fccd58a8687d10c544477274b
alexey-belov1 <veshousis1@gmail.com>
2020-04-17 19:12:06 +0500

    Jun. Часть 004. 1. Хранилище продуктов (2)

--- chapter_004_jun/src/main/java/ru/job4j/lsp/ControllQuality.java
+++ chapter_004_jun/src/main/java/ru/job4j/lsp/ControllQuality.java
@@ -1,6 +1,7 @@
 package ru.job4j.lsp;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
@@ -15,6 +16,18 @@ public class ControllQuality {
             }
         }
 
+        toStorage(map, foods);
+    }
+
+    public void distribute(Map<Storage, PutInStorage> map, Food food) {
+        toStorage(map, new LinkedList<>(List.of(food)));
+    }
+
+    public void distribute(Map<Storage, PutInStorage> map, List<Food> foods) {
+        toStorage(map, new LinkedList<>(foods));
+    }
+
+    private void toStorage(Map<Storage, PutInStorage> map, Queue<Food> foods) {
         Food food;
         while ((food = foods.poll()) != null) {
             for (Storage storage : map.keySet()) {
--- chapter_004_jun/src/test/java/ru/job4j/lsp/ControllQualityTest.java
+++ chapter_004_jun/src/test/java/ru/job4j/lsp/ControllQualityTest.java
@@ -3,40 +3,97 @@ package ru.job4j.lsp;
 import java.time.LocalDate;
 import java.util.*;
 
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ControllQualityTest {
 
+    private Food cheese, milk, banana, coockie;
+
+    @Before
+    public void init() {
+        LocalDate now = LocalDate.now();
+        Date created = java.sql.Date.valueOf(now.minusDays(10));
+        Date expaired = java.sql.Date.valueOf(now.plusDays(10));
+        cheese = new Food("cheese", created, expaired, 300.0, 0);
+
+        expaired = java.sql.Date.valueOf(now.minusDays(1));
+        milk = new Food("milk", created, expaired, 40.0, 0);
+
+        expaired = java.sql.Date.valueOf(now.plusDays(2));
+        banana = new Food("banana", created, expaired, 120.0, 0);
+
+        expaired = java.sql.Date.valueOf(now.plusDays(60));
+        coockie = new Food("cookie", created, expaired, 70.0, 0);
+    }
+
     @Test
-    public void whenControlWithoutDiscount() {
+    public void whenRedistribute() {
         Storage warehouse = new Warehouse();
         Storage shop = new Shop();
         Storage trash = new Trash();
 
-        LocalDate now = LocalDate.now();
-        Date created = java.sql.Date.valueOf(now.minusDays(10));
-        Date expaired = java.sql.Date.valueOf(now.plusDays(10));
-        warehouse.offer(new Food("cheese", created, expaired, 300.0, 0));
+        warehouse.offer(cheese);
+        shop.offer(milk);
+        trash.offer(banana);
+        trash.offer(coockie);
 
-        expaired = java.sql.Date.valueOf(now.minusDays(1));
-        shop.offer(new Food("milk", created, expaired, 40.0, 0));
-
-        expaired = java.sql.Date.valueOf(now.plusDays(2));
-        trash.offer(new Food("banana", created, expaired, 120.0, 0));
-
-        expaired = java.sql.Date.valueOf(now.plusDays(60));
-        trash.offer(new Food("cookie", created, expaired, 70.0, 0));
-
-        Map<Storage, PutInStorage> map = new HashMap<>() {{
+        Map<Storage, PutInStorage> map = new HashMap<>() { {
             put(warehouse, new PutInWarehouse());
             put(shop, new PutInShop());
             put(trash, new PutInTrash());
-        }};
+        } };
 
         new ControllQuality().redistribute(map);
+
+        List<Food> inShop = new ArrayList<>(List.of(shop.poll(), shop.poll()));
+        inShop.sort(Comparator.comparing(Food::getName));
+
+        assertThat(warehouse.poll().getName(), is("cookie"));
+        assertThat(inShop.get(0).getName(), is("banana"));
+        assertThat(inShop.get(0).getDiscount(), is(20.0));
+        assertThat(inShop.get(1).getName(), is("cheese"));
+        assertThat(trash.poll().getName(), is("milk"));
+    }
+
+    @Test
+    public void whenDistributeFood() {
+        Storage warehouse = new Warehouse();
+        Storage shop = new Shop();
+        Storage trash = new Trash();
+
+        Map<Storage, PutInStorage> map = new HashMap<>() { {
+            put(warehouse, new PutInWarehouse());
+            put(shop, new PutInShop());
+            put(trash, new PutInTrash());
+        } };
+
+        new ControllQuality().distribute(map, cheese);
+
+        assertThat(warehouse.poll(), is(nullValue()));
+        assertThat(shop.poll().getName(), is("cheese"));
+        assertThat(trash.poll(), is(nullValue()));
+    }
+
+    @Test
+    public void whenDistributeListFood() {
+        Storage warehouse = new Warehouse();
+        Storage shop = new Shop();
+        Storage trash = new Trash();
+
+        Map<Storage, PutInStorage> map = new HashMap<>() { {
+            put(warehouse, new PutInWarehouse());
+            put(shop, new PutInShop());
+            put(trash, new PutInTrash());
+        } };
+
+        List<Food> foods = List.of(cheese, milk, banana, coockie);
+
+        new ControllQuality().distribute(map, foods);
 
         List<Food> inShop = new ArrayList<>(List.of(shop.poll(), shop.poll()));
         inShop.sort(Comparator.comparing(Food::getName));
