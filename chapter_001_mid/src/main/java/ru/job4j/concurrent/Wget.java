8e56d295c938df683ca44549949e58d6473f3715
alexey-belov1 <veshousis1@gmail.com>
2020-05-09 17:13:42 +0500

    Mid. Часть 001. 1.2. Режим ожидания

---
+++ chapter_001_mid/src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    System.out.println("Start loading ... ");
+                    try {
+                        for (int index = 0; index <= 100; index++) {
+                            System.out.print("\rLoading : " + index  + "%");
+                            Thread.sleep(1000);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("\nLoaded.");
+                }
+        );
+        thread.start();
+    }
+}
